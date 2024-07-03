ce9227360bc4c95bfd3a219b9164beb6693d6b2f
Anderson <sony.sa79@gmail.com>
2024-07-03 15:04:48 -0300

    first commit

---
+++ ConvencoesDeVariaveis.java
@@ -0,0 +1,27 @@
+public class ConvencoesDeVariaveis {
+    public static void main (String[] args) {
+
+
+
+        // Camel Case
+        int idadePessoa = 25;
+        double salarioAnual = 50000.0;
+
+        // Nomes Significativos
+        String nomeCompleto = "João da Silva";
+        boolean isEmpregado = true;
+
+        // Constantes
+        final double PI = 3.14159;
+        final int MAX_TENTATIVAS = 5;
+
+        // Exibindo os valores
+        System.out.println("Idade: " + idadePessoa);
+        System.out.println("Salário Anual: " + salarioAnual);
+        System.out.println("Nome Completo:" + nomeCompleto);
+        System.out.println("Empregado:" + isEmpregado);
+        System.out.println("PI:" + PI);
+        System.out.println("Máximo de Tentativas: " + MAX_TENTATIVAS);
+ 
+    }
+}
---
+++ TiposPrimitivos.java
@@ -0,0 +1,36 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // Números Inteiros
+        byte numeroByte = 100;
+        short numeroShort = 10000;
+        int numeroInt = 100000;
+        long numeroLong = 10000000000L;
+
+        // Número de Ponto Flutuante
+        float numeroFloat = 10.99f;
+        double numeroDouble = 10.99d;
+
+        // Caracteres
+        char caractere = 'A';
+
+        // Booleanos
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+
+
+        // Exibindo os valores
+        System.out.println("byte: " + numeroByte);
+        System.out.println("short: " + numeroShort);
+        System.out.println("int" + numeroInt);
+        System.out.println("long" + numeroLong);
+        System.out.println("float" + numeroFloat);
+        System.out.println("double" + numeroDouble);
+        System.out.println("char" + caractere);
+        System.out.println("boolean verdadeiro:" + verdadeiro);
+        System.out.println("boolean falson:" + falso);
+        
+
+
+    }
+}
